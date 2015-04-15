675175e907061179b39ce5094a0c06bb9ec1f66b
tratost <tratos@msn.com>
2015-04-15 17:59:14 +0200

    add few files for vue

---
+++ src/engine/VEntity.java
@@ -0,0 +1,13 @@
+package engine;
+
+import javax.swing.*;
+
+/**
+ * Created by tratost on 15/04/15.
+ */
+public class VEntity extends JPanel {
+
+    public VEntity() {
+        super();
+    }
+}
---
+++ src/graphics/VMainMenu.java
@@ -0,0 +1,13 @@
+package graphics;
+
+import javax.swing.*;
+
+/**
+ * Created by tratost on 15/04/15.
+ */
+public class VMainMenu extends JPanel{
+
+    public VMainMenu() {
+        super();
+    }
+}
