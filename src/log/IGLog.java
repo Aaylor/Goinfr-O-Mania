9fec3ede4c20bd2d834cf50e37a60ca9347f1ed5
aaylor <loic.runarvot@gmail.com>
2015-04-16 15:45:12 +0200

    IGLog.java: fix colors.

--- src/log/IGLog.java
+++ src/log/IGLog.java
@@ -5,9 +5,9 @@ import java.io.Console;
 public class IGLog {
 
     private static final Console CONSOLE = System.console();
-    private static final String  YELLOW  = "\\033[1;33m";
-    private static final String  RED     = "\\033[0;31m";
-    private static final String  CLEAR   = "\\033[0m";
+    private static final String  YELLOW  = "\033[1;33m";
+    private static final String  RED     = "\033[0;31m";
+    private static final String  CLEAR   = "\033[0m";
 
     private static String addColors(String color, String message) {
         String useColor;
