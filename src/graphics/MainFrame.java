f21f93ea37cf34e25b4d59720a6d40ab5f54252e
aaylor <loic.runarvot@gmail.com>
2015-04-16 14:08:46 +0200

    MainFrame.java: launch with the correct bundle.

--- src/graphics/MainFrame.java
+++ src/graphics/MainFrame.java
@@ -2,21 +2,29 @@ package graphics;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ResourceBundle;
 
 /**
  * Created by tratost on 15/04/15.
  */
 public class MainFrame extends JFrame {
 
-    public MainFrame() throws HeadlessException {
-        super("All'a bataille !!");
+    public MainFrame(ResourceBundle bundle) throws HeadlessException {
+        super(bundle.getString("title"));
+        defaultFrameConfiguration();
+    }
+
+    private void defaultFrameConfiguration() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new MainFrame();
+                ResourceBundle bundle =
+                        ResourceBundle.getBundle("lang/bundle");
+                new MainFrame(bundle);
             }
         });
     }
