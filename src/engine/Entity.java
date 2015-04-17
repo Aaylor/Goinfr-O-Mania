22756b219e78729724149a5a143c6d3307920412
aaylor <loic.runarvot@gmail.com>
2015-04-17 19:16:32 +0200

    Entity.java: an entity now allow to have a position and a size. EntityManager.java: add documentation + implements functions.

--- src/engine/Entity.java
+++ src/engine/Entity.java
@@ -1,15 +1,27 @@
 package engine;
 
+import java.awt.*;
 import java.util.Observable;
 
-/**
- * Created by tratost on 13/04/15.
- */
 public abstract class Entity extends Observable {
+
+    private Point position;
+    private Dimension size;
 
     /* Constructors */
 
     public Entity() {}
+
+    /**
+     * Construct an Entity with its start position (in x,y coordinates) and
+     * its box size (as a rectangular shape).
+     * @param startPosition
+     * @param size
+     */
+    public Entity(Point startPosition, Dimension size) {
+        this.position = startPosition;
+        this.size     = size;
+    }
 
 
     /* Entity Abstract Functionalities */
--- src/engine/EntityManager.java
+++ src/engine/EntityManager.java
@@ -1,32 +1,68 @@
 package engine;
 
-import java.util.Set;
+import java.util.LinkedList;
+
 
 /**
- * Created by tratost on 17/04/15.
+ *  Manage every created entities.
  */
 public class EntityManager {
 
-    Set<Entity> entities;
+    LinkedList<Entity> entities;
 
+    /**
+     *  Create an empty manager.
+     */
     public EntityManager(){
+        entities = new LinkedList<>();
+    }
 
+    /**
+     *  Create a new manager with a number of default Entities.
+     *  @param defaultEntities Default entities to add.
+     */
+    public EntityManager(LinkedList<Entity> defaultEntities) {
+        entities = (LinkedList<Entity>) defaultEntities.clone();
     }
 
     public void initialize(){
 
     }
 
-    public boolean detectCollision(){
+    public void entityLoop() {
+
+    }
+
+    public boolean detectCollisions(){
         return false;
     }
 
+    /**
+     *  Check if two entities are in collision.
+     *  @param e1 The first entity.
+     *  @param e2 The second entity.
+     *  @return True if they collided.
+     */
+    public boolean collision(Entity e1, Entity e2) {
+        return false;
+    }
+
+    /**
+     *  Add a new entity into the manager.
+     *  @param e The entity to add.
+     */
     public void addEntity(Entity e){
-
+        entities.addFirst(e);
     }
 
-    public boolean rmEntity(Entity e){
-        return false;
+    /**
+     *  Remove entity of the manager.
+     *  If the entity doesn't exists, the function returns false.
+     *  @param e The entity to remove.
+     *  @return True if entity has been removed correctly.
+     */
+    public boolean removeEntity(Entity e){
+        return entities.remove(e);
     }
 
 }
