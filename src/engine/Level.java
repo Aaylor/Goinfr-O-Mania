3dd32e097e2026f0c66a847372a3291ec13f221a
aaylor <loic.runarvot@gmail.com>
2015-04-24 19:19:55 +0200

    Level: is no longer part of MVC pattern

--- src/engine/Level.java
+++ src/engine/Level.java
@@ -11,14 +11,20 @@ public class Level {
     private EntityManager em;
     private SoundManager sm;
     private MSound actualMusic;
-    private Board board;
 
-    public Level(Score actualScore, Goal goal, EntityManager em, SoundManager sm, MSound music, Board board) {
+    public Level(Score actualScore, Goal goal, EntityManager em, SoundManager sm) {
         this.actualScore = actualScore;
         this.goal = goal;
         this.em = em;
         this.sm = sm;
-        this.actualMusic = music;
-        this.board = board;
     }
+
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public void gameLoop() {
+        em.entityLoop();
+    }
+
 }
