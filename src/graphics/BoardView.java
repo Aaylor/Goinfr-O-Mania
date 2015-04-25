64ece659d85a1d723fcc38c9908264ebe1b872fa
aaylor <loic.runarvot@gmail.com>
2015-04-26 00:28:06 +0200

    BoardView.java: use AffineTransform to get better performance drawing images.

--- src/graphics/BoardView.java
+++ src/graphics/BoardView.java
@@ -3,10 +3,10 @@ package graphics;
 import engine.Entity;
 import engine.EntityManager;
 import engine.EntityView;
-import engine.Level;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.Collection;
 import java.util.Observable;
 import java.util.Observer;
@@ -29,29 +29,39 @@ public class BoardView extends JPanel implements Observer {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        System.out.println("repaint");
-
         Graphics2D graphics2D = (Graphics2D)g;
 
         EntityManager manager = board.getLevel().getEntityManager();
 
         EntityView gluttonView = manager.getGluttonView();
         Entity glutton = gluttonView.getEntity();
-        graphics2D.drawImage(gluttonView.getCurrentDrawing(), null,
-                glutton.getX(), glutton.getY());
+
+        AffineTransform t = new AffineTransform();
+        t.translate(glutton.getX(), glutton.getY());
+        t.scale(1, 1);
+
+        graphics2D.drawImage(gluttonView.getCurrentDrawing(), t, null);
 
         Collection<EntityView> nutritionists = manager.getNutritionistsView();
         for (EntityView e : nutritionists) {
             Entity entity = e.getEntity();
-            graphics2D.drawImage(e.getCurrentDrawing(), null,
-                    entity.getX(), entity.getY());
+
+            AffineTransform nt = new AffineTransform();
+            nt.translate(entity.getX(), entity.getY());
+            nt.scale(1, 1);
+
+            graphics2D.drawImage(e.getCurrentDrawing(), nt, null);
         }
 
         Collection<EntityView> others = manager.getOthersView();
         for (EntityView e : others) {
             Entity entity = e.getEntity();
-            graphics2D.drawImage(e.getCurrentDrawing(), null,
-                    entity.getX(), entity.getY());
+
+            AffineTransform ot = new AffineTransform();
+            ot.translate(entity.getX(), entity.getY());
+            ot.scale(1, 1);
+
+            graphics2D.drawImage(e.getCurrentDrawing(), ot, null);
         }
     }
 }
