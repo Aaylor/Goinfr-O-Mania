baf74fcf8bba49c132226663024b84809c740295
aaylor <loic.runarvot@gmail.com>
2015-05-18 13:18:50 +0200

    ArtificialIntelligence.java: next step doesn't take any manager now.

--- src/engine/nutritionists/ArtificialIntelligence.java
+++ src/engine/nutritionists/ArtificialIntelligence.java
@@ -1,7 +1,5 @@
 package engine.nutritionists;
 
-import engine.EntityManager;
-
 public interface ArtificialIntelligence {
-    public void nextStep(EntityManager manager);
+    public void nextStep();
 }
