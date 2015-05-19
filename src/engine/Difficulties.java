99e2355f8124cbc0a63e49717adeb0844f5b7f1f
aaylor <loic.runarvot@gmail.com>
2015-05-20 01:26:21 +0200

    Difficulties.java: is now an enumeration.

--- src/engine/Difficulties.java
+++ src/engine/Difficulties.java
@@ -1,31 +1,9 @@
 package engine;
 
-/**
- * Created by tratost on 13/04/15.
- */
-public class Difficulties {
+public enum Difficulties {
 
-    private int value;
+    EASY,
+    MEDIUM,
+    HARD
 
-    //CONSTRUCTORS
-
-    public Difficulties(int value) {
-        this.value = value;
-    }
-
-    public Difficulties() {
-        this.value = 1;
-    }
-
-    //SETTERS
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    //GETTERS
-
-    public int getValue() {
-        return value;
-    }
 }
