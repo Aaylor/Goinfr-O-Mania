6a3b52f4f866fc67b474722c6e940b34bff91725
aaylor <loic.runarvot@gmail.com>
2015-05-21 16:34:10 +0200

    Score.java: update.

--- src/engine/Score.java
+++ src/engine/Score.java
@@ -2,44 +2,31 @@ package engine;
 
 import java.util.Observable;
 
-/**
- * Created by tratost on 13/04/15.
- */
-public class Score extends Observable {
-    //Plutot un MPlayer ?
-    private String name;
+public class Score {
     private int value;
 
 
     //CONSTRUCTORS
 
     public Score(String name, int value) {
-        this.name = name;
         this.value = value;
     }
 
     public Score() {
-        this.name = "Tester";
         this.value = 0;
     }
 
     //SETTERS
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setValue(int value) {
         this.value = value;
-        setChanged();
-        notifyObservers();
+    }
+
+    public void add(int value) {
+        this.value += value;
     }
 
     //GETTERS
-
-    public String getName() {
-        return name;
-    }
 
     public int getValue() {
         return value;
