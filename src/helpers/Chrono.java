eb9368391b97e1b30366fc73d39958894367f8d5
aaylor <loic.runarvot@gmail.com>
2015-05-21 12:38:04 +0200

    Chrono.java: create a way to have a chrono for the game.

---
+++ src/helpers/Chrono.java
@@ -0,0 +1,61 @@
+package helpers;
+
+public class Chrono {
+
+    private static long startTime;
+    private static long startPause;
+    private static long totalPause;
+
+    public Chrono() {
+        reinitialization();
+    }
+
+    public void reinitialization() {
+        startTime  = 0;
+        startPause = 0;
+        totalPause = 0;
+    }
+
+    public void start() {
+        reinitialization();
+        startTime = System.currentTimeMillis();
+    }
+
+    public void resume() {
+        if (startTime == 0 || startPause == 0) {
+            /* Has not be started */
+            return;
+        }
+
+        totalPause += System.currentTimeMillis() - startPause;
+    }
+
+    public void pause() {
+        if (startTime == 0) {
+            /* Has not be started */
+            return;
+        }
+
+        startPause = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        reinitialization();
+    }
+
+    public long timeEllapsed() {
+        return System.currentTimeMillis() - startTime - totalPause;
+    }
+
+    @Override
+    public String toString() {
+        long realTime = timeEllapsed();
+
+        int minutes      = (int) ((realTime / 1000) / 60);
+        int seconds      = (int) ((realTime / 1000) % 60);
+        int milliseconds = (int) ((realTime / 100)  % 10);
+
+        return String.format("%02d:%02d:%d", minutes, seconds, milliseconds);
+    }
+
+}
